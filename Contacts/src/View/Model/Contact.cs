2fcf47cb536775a36ffe1fb25a5fc4b0a633c52d
Wismut8 <bosyakova.liza@mail.ru>
2025-03-18 22:30:36 +0700

    feature: create the MainWindow interface

---
+++ Contacts/src/View/Model/Contact.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace View.Model
+{
+    /// <summary>
+    /// Контакт.
+    /// </summary>
+    internal class Contact
+    {
+        /// <summary>
+        /// Хранит полное имя (Фамилия Имя Отчество).
+        /// </summary>
+        private string _fullName;
+        /// <summary>
+        /// Хранит номер телефона.
+        /// </summary>
+        private string _phoneNumber;
+        /// <summary>
+        /// Хранит электронную почту.
+        /// </summary>
+        private string _email;
+
+
+        /// <summary>
+        /// Возвращает и задает полное имя покупателя.
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона.
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Возвращает и задает электронную почту.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Создает экземпляр контакта. <see cref="Contact"/>
+        /// </summary>
+        /// <param name="fullName">Полное имя.</param>
+        /// <param name="phoneNumber">Телефонный номер.</param>
+        /// <param name="email">Электронная почта.</param>
+        public Contact(string fullName, string phoneNumber, string email)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        /// <summary>
+        /// Конструктор по умолчанию.
+        /// </summary>
+        public Contact() { }
+
+    }
+}
